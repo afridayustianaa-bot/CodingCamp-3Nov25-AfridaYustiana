a2c796fef52d92c8ac82af6378164979e9ddc556
Afrida Yustiana <afrida.yustianaa@gmail.com>
2025-11-08 09:57:16 +0700

    #Mini Project

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-welcomeMessage();
-function welcomeMessage() {
-    let name = prompt("Please enter your name:");
-
-    if (name != null) {
-
-    document.getElementById('welcome-speech').innerHTML = 'Hello ' + name +', welcome to our website!';
-    }
-}
 
 function validateForm() {
     // Ambil nilai input
@@ -36,7 +27,7 @@ function validateForm() {
         return false;
     }
 
-    // ✅ Jika lulus validasi → tampilkan output
+    // Jika lulus validasi maka tampilkan output
     document.getElementById("rNama").innerText = nama;
     document.getElementById("rTanggal").innerText = tanggal;
     document.getElementById("rGender").innerText = gender.value;
@@ -47,3 +38,68 @@ function validateForm() {
 
     return false; // cegah reload halaman
 }
+
+    // Menampilkan tahun saat ini di footer
+  document.getElementById("footerText").innerHTML =
+    "&copy; " + new Date().getFullYear() + " ConstructPro. Hak Cipta Dilindungi.";
+
+    //  Validasi formulir kontak
+  const form = document.getElementById("contactForm");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Ambil nilai input
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    // Elemen error
+    let errName = document.getElementById("errName");
+    let errEmail = document.getElementById("errEmail");
+    let errMessage = document.getElementById("errMessage");
+
+    // Kosongkan error sebelum validasi ulang
+    errName.textContent = "";
+    errEmail.textContent = "";
+    errMessage.textContent = "";
+
+    let valid = true;
+
+    // Validasi sesuai aturan Zod
+    if (name.length < 2) {
+      errName.textContent = "Nama harus minimal 2 karakter.";
+      valid = false;
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+      errEmail.textContent = "Masukkan email yang valid.";
+      valid = false;
+    }
+
+    if (message.length < 10) {
+      errMessage.textContent = "Pesan harus minimal 10 karakter.";
+      valid = false;
+    }
+
+    if (!valid) return;
+
+    // Jika valid maka submit form dan reset form
+    showToast();
+    form.reset();
+  });
+
+    // 
+  function goToContact() {
+    location.href = "#message-page";
+  }
+
+    const words = ["Tukang Bangunan", "Renovasi Rumah", "Pemasangan Keramik", "Finishing & Pengecatan"];
+  let index = 0;
+
+  function changeText() {
+    document.getElementById("dynamicText").innerText = words[index];
+    index = (index + 1) % words.length;
+  }
+
+  setInterval(changeText, 2000);
